6d67ab693df53be01aed11f08d337fd87c67fbd8
nadinibyant <2111522021_nadini@student.unand.ac.id>
2022-04-29 17:03:32 +0700

    MouseCode

---
+++ assets/js/index.js
@@ -0,0 +1,47 @@
+function html() {
+    document.querySelector(".html").src = "assets/image/html2.png";
+}
+
+function html2() {
+    document.querySelector(".html").src = "assets/image/html.png";
+}
+
+function js() {
+    document.querySelector(".js").src = "assets/image/js2.png";
+}
+
+function js2() {
+    document.querySelector(".js").src = "assets/image/js.png";
+}
+
+function css() {
+    document.querySelector(".css").src = "assets/image/css2.png";
+}
+
+function css2() {
+    document.querySelector(".css").src = "assets/image/css.png";
+}
+
+function php() {
+    document.querySelector(".php").src = "assets/image/php2.png";
+}
+
+function php2() {
+    document.querySelector(".php").src = "assets/image/php.jpg";
+}
+
+function mysql() {
+    document.querySelector(".mysql").src = "assets/image/mysql2.png";
+}
+
+function mysql2() {
+    document.querySelector(".mysql").src = "assets/image/mysql.png";
+}
+
+function java() {
+    document.querySelector(".java").src = "assets/image/java2.png";
+}
+
+function java2() {
+    document.querySelector(".java").src = "assets/image/java.png";
+}
